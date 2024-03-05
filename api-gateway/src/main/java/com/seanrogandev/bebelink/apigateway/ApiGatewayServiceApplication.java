d4c4bd7b44340aa32bfffa3e0dafe1a293a791b2
Sean Rogan <74671024+SeanRogan@users.noreply.github.com>
2024-03-05 17:35:36 -0500

    configured to work with existing microservices

--- api-gateway/src/main/java/com/seanrogandev/bebelink/apigateway/ApiGatewayServiceApplication.java
+++ api-gateway/src/main/java/com/seanrogandev/bebelink/apigateway/ApiGatewayServiceApplication.java
@@ -2,6 +2,7 @@ package com.seanrogandev.bebelink.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
