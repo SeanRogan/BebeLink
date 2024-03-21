dd774cd969246ff8358018cd826b75900900abd7
Sean Rogan <74671024+SeanRogan@users.noreply.github.com>
2024-03-20 21:32:39 -0400

    clean up

--- api-gateway/src/main/java/com/seanrogandev/bebelink/apigateway/kafka/KafkaProducerConfig.java
+++ api-gateway/src/main/java/com/seanrogandev/bebelink/apigateway/kafka/KafkaProducerConfig.java
@@ -1,11 +1,12 @@
 package com.seanrogandev.bebelink.apigateway.kafka;
 
 import com.seanrogandev.bebelink.apigateway.model.RoutingEvent;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import reactor.kafka.sender.SenderOptions;
 
@@ -15,11 +16,10 @@ import java.util.Map;
 @Configuration
 
 public class KafkaProducerConfig {
-    @Value("${spring.kafka.producer.bootstrap-servers}")
-    private String bootstrapServers;
-
     @Value("${spring.kafka.producer.acks}")
     private String acks;
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
     @Value("${spring.kafka.producer.key-serializer}")
     private String keySerializer;
     @Value("${spring.kafka.producer.value-serializer}")
@@ -34,16 +34,27 @@ public class KafkaProducerConfig {
     @Bean
     public ReactiveKafkaProducerTemplate<String, RoutingEvent> kafkaProducerTemplate() {
         Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.ACKS_CONFIG, acks);
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
-        props.put(ProducerConfig.ACKS_CONFIG, acks);
-        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
         props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
         props.put(ProducerConfig.LINGER_MS_CONFIG, linger);
         props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
-
         props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5);
+
         return new ReactiveKafkaProducerTemplate<>(SenderOptions.create(props));
     }
+
+    @Bean
+    public NewTopic createRoutingTopic() {
+        return (TopicBuilder.name("routing-event-topic")
+                .partitions(3)
+                .replicas(3)
+                .configs(Map.of("min.insync.replicas", "2"))
+                .build());
+    }
+
+
 }
